862249d1f4bdd1e976c8ce9a3076befc8aaad076
Pallabigope <pallavigope2019@gmail.com>
2024-02-07 17:22:02 +0530

    This is a basic one.

---
+++ app/src/main/java/com/example/compose2extended/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.compose2extended.di
+
+import com.example.compose2extended.data.model.base_url
+import com.example.compose2extended.data.model.movieInterface
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule{
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(base_url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providemovieinterface(retrofit: Retrofit): movieInterface {
+        return retrofit.create(movieInterface::class.java)
+    }
+}
